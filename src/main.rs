74b7516146732b0de699ea96065d19736d1123bd
k-avy <kavyuushukla59@gmail.com>
2024-07-05 17:57:07 +0530

    added the code for testing the database query in rusqlite and tide.

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,16 @@
 // mod tidensled;
 // mod tiny_sqlx;
 // mod db_insert;
-mod tide_db;
+// mod tide_db;
+mod tide_qury;
+
 // use threadpooling::tiny_fn;
 // use server_db_pool::start_server;
 // use tiny_sqlx::tinysqlserver;
 // use tidensled::tidendb;
 // use db_insert::tiny_fn;
-use tide_db::tide_fn;
+// use tide_db::tide_fn;
+use tide_qury::tide_fn;
 fn main() {
 
    async_std::task::block_on(tide_fn());
