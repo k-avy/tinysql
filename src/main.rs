65766e11a0febd376ac61ea4803fad71c5801c3a
k-avy <kavyuushukla59@gmail.com>
2024-07-05 17:29:37 +0530

    added tide and rusqlite code for analysis.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
-mod threadpooling;
-// mod server_db_pool;
-mod tidensled;
-mod tiny_sqlx;
-mod db_insert;
+// mod threadpooling;
+// // mod server_db_pool;
+// mod tidensled;
+// mod tiny_sqlx;
+// mod db_insert;
+mod tide_db;
 // use threadpooling::tiny_fn;
 // use server_db_pool::start_server;
 // use tiny_sqlx::tinysqlserver;
 // use tidensled::tidendb;
-use db_insert::tiny_fn;
+// use db_insert::tiny_fn;
+use tide_db::tide_fn;
 fn main() {
-   tiny_fn();
+
+   async_std::task::block_on(tide_fn());
+
 //    start_server();
     
 //    async_std::task::block_on(tinysqlserver()).unwrap();
